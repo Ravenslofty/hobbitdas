c310cdf29b1b87aa6cebf1f3336c5e64da06f0d8
Lofty <dan.ravensloft@gmail.com>
2024-06-18 21:24:30 +0100

    fix endianness

--- access.c
+++ access.c
@@ -9,6 +9,6 @@ get4(Map *map, ulong addr, int space, ulong *x)
     // get4 calls a lot of nested functions, simply to read 4 bytes.
     // - lofty
     USED(space);
-    *x = map->data[addr+3] | (map->data[addr+2] << 8) | (map->data[addr+1] << 16) | ((ulong)(map->data[addr]) << 24);
+    *x = map->data[addr+0] | (map->data[addr+1] << 8) | (map->data[addr+2] << 16) | ((ulong)(map->data[addr+3]) << 24);
     return 1;
 }
