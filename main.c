70f7d5fdc7ba585690bc41d1d8493b9f962ceac9
Lofty <dan.ravensloft@gmail.com>
2024-06-18 18:40:32 +0100

    now uses mmap to disassemble files

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "u.h"
 #include "mach.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 extern void hobbitprintins(Map *, char, int);
 
@@ -25,10 +30,37 @@ const char isqrt[0x1a + 2] = {
     0x08, 0x06
 };
 
-int main() {
+int main(int argc, char **argv) {
+    int fd = -1;
+
+    if (argc != 3) {
+        printf("usage: %s file-to-disassemble offset\n", argv[0]);
+        return -1;
+    }
+    
+    int offset;
+    if ((offset = atoi(argv[2])) < 2) {
+        printf("offset must be at least 2, because the disassembler looks backwards one instruction\n");
+        return -1;
+    }
+
+    if ((fd = open(argv[1], O_RDONLY, 0)) == -1) {
+        perror("open failed");
+        return -1;
+    }
+
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        perror("fstat failed");
+        return -1;
+    }
+
     Map map;
-    map.data = malloc(0x1a + 2);
-    memcpy(map.data, &isqrt, 0x1a + 2);
+    if ((map.data = (char*)mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+        perror("mmap failed");
+        return -1;
+    }
+
     dot = 2;
     dotinc = 0;
     do {
@@ -36,6 +68,7 @@ int main() {
         hobbitprintins(&map, 0, 0x00000000);
         printf("\n");
         dot += dotinc;
-    } while (dot != 0x1a + 4);
-    free(map.data);
+    } while (dot != st.st_size);
+
+    return 0;
 }
